267ec325627cbfe59b20326fb7369a71a0f5e775
louis0113 <luizhenrique011304@gmail.com>
2026-05-22 17:37:25 -0300

    feat: first files

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    <title>fbskhu</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">	  <link type="text/css" rel="stylesheet" href="styles/styles.css" />
+    </head>
+
+        <body>
+<?php 
+        $hello = "Olá Mundo";
+        echo "<h1>$hello</h1>";
+?>
+
+        </body>
+</html>
